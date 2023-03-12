d169e586f8bce78bb4c3a774854c0c9877c7c911
Nikola Whallon <browncanstudios@gmail.com>
2023-03-11 23:10:47 -0800

    added text box

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -1,41 +1,26 @@
 extends Node
 
-var level = 1
-
-func _input(_event):
-	if Input.is_action_just_pressed("bomb"):
-		var lit_bomb = load("res://Scenes/BombLit.tscn").instance()
-		if $Player.direction == Vector2.LEFT:
-			lit_bomb.global_position = $Player.global_position + Vector2(-16, -8)
-			lit_bomb.apply_impulse(Vector2.ZERO, Vector2(-100, -100))
-		elif $Player.direction == Vector2.RIGHT:
-			lit_bomb.global_position = $Player.global_position + Vector2(16, -8)
-			lit_bomb.apply_impulse(Vector2.ZERO, Vector2(100, -100))
-		add_child(lit_bomb)
+var level = 0
 
 func _ready():
-	var new_level = load("res://Scenes/Level1.tscn").instance()
-	add_child(new_level)
-	
-	$Player.global_position = Vector2(70, 230)
-	
-	$Player/Camera2D.limit_bottom = 260
-	$Player/Camera2D.limit_left = 0
-	$Player/Camera2D.limit_right = 476
+	# TODO: instead of loading level 1, load the initial (cut)scene
+	level += 1
+	load_level("res://Scenes/Level1.tscn")
 
-func _on_Player_wake_up():
+func load_level(path):
+	print("loading level " + path)
+	
 	var old_levels = get_tree().get_nodes_in_group("Level")
 	for old_level in old_levels:
 		old_level.queue_free()
-	
-	level += 1
-	
-	if level == 2:
-		var new_level = load("res://Scenes/Level2.tscn").instance()
-		add_child(new_level)
 
-		$Player.global_position = Vector2(70, 230)
-	
-		$Player/Camera2D.limit_bottom = 260
-		$Player/Camera2D.limit_left = 0
-		$Player/Camera2D.limit_right = 504
+	var new_level = load(path).instance()
+	add_child(new_level)
+	new_level.get_node("Player").connect("wake_up", self, "_on_Player_wake_up")
+
+func _on_Player_wake_up():
+	if level == 1:
+		level += 1
+		load_level("res://Scenes/Level2.tscn")
+	if level == 2:
+		print("go to final scene?")
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -2,23 +2,43 @@ extends KinematicBody2D
 
 signal wake_up
 
+export var camera_limit_bottom = 10000000
+export var camera_limit_left = -10000000
+export var camera_limit_right = 10000000
 export var gravity = 10
 export var run_speed = 100
 export var jump_speed = 250
 var velocity = Vector2.ZERO
 var direction = Vector2.LEFT
+var input_disabled = false
+
+func _ready():
+	$Camera2D.limit_bottom = camera_limit_bottom
+	$Camera2D.limit_left = camera_limit_left
+	$Camera2D.limit_right = camera_limit_right
+
+func _input(_event):
+	if Input.is_action_just_pressed("bomb") and !input_disabled:
+		var lit_bomb = load("res://Scenes/BombLit.tscn").instance()
+		if direction == Vector2.LEFT:
+			lit_bomb.global_position = global_position + Vector2(-16, -8)
+			lit_bomb.apply_impulse(Vector2.ZERO, Vector2(-100, -100))
+		elif direction == Vector2.RIGHT:
+			lit_bomb.global_position = global_position + Vector2(16, -8)
+			lit_bomb.apply_impulse(Vector2.ZERO, Vector2(100, -100))
+		get_parent().add_child(lit_bomb)
 
 func _physics_process(_delta):
 	if !is_on_floor():
 		velocity.y += gravity
 
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and !input_disabled:
 		if is_on_floor():
 			velocity.y -= jump_speed
 			
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(KEY_A) and !input_disabled:
 		velocity.x = -run_speed
-	elif Input.is_key_pressed(KEY_D):
+	elif Input.is_key_pressed(KEY_D) and !input_disabled:
 		velocity.x = run_speed
 	else:
 		velocity.x = 0
