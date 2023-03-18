1bedefaa6691dada39a0b002de6c25bfd7696ab2
Nikola Whallon <browncanstudios@gmail.com>
2023-03-17 18:26:02 -0700

    bomb explosion animation and refined collision logic

--- Scenes/BombLit.gd
+++ Scenes/BombLit.gd
@@ -3,10 +3,22 @@ extends RigidBody2D
 func destroy():
 	get_tree().queue_delete(self)
 
+func explode():
+	set_gravity_scale(0.0)
+	set_linear_damp(1000000)
+	set_collision_layer_bit(1, false)
+	set_collision_mask_bit(1, false)
+	$Sprite.visible = false
+	$AnimatedSprite.visible = true
+	$AnimatedSprite.play("explosion")
+
+func _on_Timer_timeout():
+	explode()
+
+func _on_AnimatedSprite_animation_finished():
+	destroy()
+
 func _on_BombLit_body_entered(body):
 	if body.is_in_group("Block"):
 		body.destroy()
-		destroy()
-
-func _on_Timer_timeout():
-	destroy()
+		explode()
